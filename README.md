07b183e6a4d77f0469bc6eb9eff9e5d455e4999d
ngshya <ngshya@gmail.com>
2019-09-05 21:39:26 +0200

    Example script.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # multiprocessing-tensorflow
 Run Tensorflow/Keras models in parallel with multiprocessing
+
+Run the example script: 
+
+```bash
+python mptf.py
+```
+
+and you should see something like:
+
+```
+Worker 1 working...
+Worker 2 working...
+Worker 1 done!
+Worker 3 working...
+Worker 2 done!
+Worker 4 working...
+Worker 3 done!
+Worker 5 working...
+Worker 4 done!
+Worker 5 done!
+['Worker 1 OK!', 'Worker 2 OK!', 'Worker 3 OK!', 'Worker 4 OK!', 'Worker 5 OK!']
+```
---
+++ mptf.py
@@ -0,0 +1,48 @@
+# Run Tensorflow/Keras models in parallel with multiprocessing
+
+
+# Import packages.
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+import multiprocessing as mp
+from tensorflow.keras.models import load_model
+import os
+
+
+# Settings
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+mp.set_start_method('spawn', force=True) # (*)
+
+
+# Function that will be executed in each worker.
+def myF(i):
+    print("Worker " + str(i) + " working...")
+    import tensorflow as tf # (*)
+    model = load_model("model.h5")
+    print("Worker " + str(i) + " done!")
+    return "Worker " + str(i) + " OK!"
+
+
+# Main 
+def main():
+    model = Sequential([
+    Dense(64, activation='relu', input_shape=(32,)),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')])
+    model.compile(optimizer=Adam(0.01),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.save("model.h5")
+    p = mp.Pool(5)
+    output = p.map(myF, [1,2,3,4,5])
+    p.close()
+    p.join()
+    print(output)
+
+
+
+if __name__ == "__main__":
+    main()
